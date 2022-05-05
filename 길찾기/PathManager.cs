4b48a1ee0ba5b4cb91f274afac80f61c6fb15792
YooJinwoo94 <71114491+YooJinwoo94@users.noreply.github.com>
2022-05-06 01:59:22 +0900

    2022_05_05
    2022_05_05

---
+++ 길찾기/PathManager.cs
@@ -0,0 +1,173 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Node
+{
+    public Node(bool _isWall, int _x, int _y) { isWall = _isWall; x = _x; y = _y; }
+
+    public bool isWall;
+    public Node ParentNode;
+
+    // G : 시작으로부터 이동했던 거리, H : |가로|+|세로| 장애물 무시하여 목표까지의 거리, F : G + H
+    public int x, y, G, H;
+    public int F { get { return G + H; } }
+}
+
+
+public class PathManager : MonoBehaviour
+{
+    [SerializeField]
+    InGameSceneCheckTargetAndGetDistance inGameSceneCheckTargetAndGetDistance;
+
+    [SerializeField]
+    Vector2Int bottomLeft, topRight, startPos, targetPos;
+
+    public List<Node> FinalNodeList;
+    [SerializeField]
+    bool allowDiagonal, dontCrossCorner;
+
+    int sizeX, sizeY;
+
+    Node[,] NodeArray;
+    Node StartNode, TargetNode, CurNode;
+    List<Node> OpenList, ClosedList;
+
+
+
+
+
+
+    public void pathFinding()
+    {
+        if (inGameSceneCheckTargetAndGetDistance.target == null) return;
+  
+        startPos =  Vector2Int.RoundToInt(this.gameObject.transform.position);
+        targetPos =  Vector2Int.RoundToInt(inGameSceneCheckTargetAndGetDistance.target.transform.position);
+
+        sizeX = topRight.x - bottomLeft.x + 1;
+        sizeY = topRight.y - bottomLeft.y + 1;
+        NodeArray = new Node[sizeX, sizeY];
+
+        for (int i = 0; i < sizeX; i++)
+        {
+            for (int j = 0; j < sizeY; j++)
+            {
+                bool isWall = false;
+                foreach (Collider2D col in Physics2D.OverlapCircleAll(new Vector2(i , j ), 0.4f))
+                {
+                    if (col.gameObject.layer == LayerMask.NameToLayer("bush")) isWall = true;
+
+
+
+                    if (col.gameObject.layer == LayerMask.NameToLayer(this.gameObject.tag))
+                    {
+                        CharState otherCharState = col.transform.GetComponentInChildren<CharState>();
+                        if (otherCharState.nowState == CharState.NowState.isReadyForAttack)
+                        {
+                            Debug.Log(col.name);
+                            isWall = true;
+                        }
+                    }
+                }
+
+                NodeArray[i, j] = new Node(isWall, i , j );
+            }
+        }
+
+        StartNode = NodeArray[startPos.x , startPos.y ];
+        TargetNode = NodeArray[targetPos.x , targetPos.y];
+
+        OpenList = new List<Node>() { StartNode };
+        ClosedList = new List<Node>();
+        FinalNodeList = new List<Node>();
+
+        while (OpenList.Count > 0)
+        {
+            CurNode = OpenList[0];
+            for (int i = 1; i < OpenList.Count; i++)
+            {
+                if (OpenList[i].F <= CurNode.F && OpenList[i].H < CurNode.H) CurNode = OpenList[i];
+            }
+
+            OpenList.Remove(CurNode);
+            ClosedList.Add(CurNode);
+
+
+            // 마지막
+            if (CurNode == TargetNode)
+            {
+                Node TargetCurNode = TargetNode;
+                while (TargetCurNode != StartNode)
+                {
+                    FinalNodeList.Add(TargetCurNode);
+                    TargetCurNode = TargetCurNode.ParentNode;
+                }
+                FinalNodeList.Add(StartNode);
+                FinalNodeList.Reverse();
+                return;
+            }
+
+
+            // ↗↖↙↘
+            if (allowDiagonal)
+            {
+                OpenListAdd(CurNode.x + 1, CurNode.y + 1);
+                OpenListAdd(CurNode.x - 1, CurNode.y + 1);
+                OpenListAdd(CurNode.x - 1, CurNode.y - 1);
+                OpenListAdd(CurNode.x + 1, CurNode.y - 1);
+            }
+
+            // ↑ → ↓ ←
+            OpenListAdd(CurNode.x, CurNode.y + 1);
+            OpenListAdd(CurNode.x + 1, CurNode.y);
+            OpenListAdd(CurNode.x, CurNode.y - 1);
+            OpenListAdd(CurNode.x - 1, CurNode.y);
+        }
+    }
+
+    void OpenListAdd(int checkX, int checkY)
+    {
+        // 상하좌우 범위를 벗어나지 않고, 벽이 아니면서, 닫힌리스트에 없다면
+        if (checkX >= bottomLeft.x && checkX < topRight.x + 1 && checkY >= bottomLeft.y && checkY < topRight.y + 1 && !NodeArray[checkX - bottomLeft.x, checkY - bottomLeft.y].isWall && !ClosedList.Contains(NodeArray[checkX - bottomLeft.x, checkY - bottomLeft.y]))
+        {
+            // 대각선 허용시, 벽 사이로 통과 안됨
+            if (allowDiagonal) if (NodeArray[CurNode.x - bottomLeft.x, checkY - bottomLeft.y].isWall && NodeArray[checkX - bottomLeft.x, CurNode.y - bottomLeft.y].isWall) return;
+
+            // 코너를 가로질러 가지 않을시, 이동 중에 수직수평 장애물이 있으면 안됨
+            if (dontCrossCorner) if (NodeArray[CurNode.x - bottomLeft.x, checkY - bottomLeft.y].isWall || NodeArray[checkX - bottomLeft.x, CurNode.y - bottomLeft.y].isWall) return;
+
+
+            // 이웃노드에 넣고, 직선은 10, 대각선은 14비용
+            Node NeighborNode = NodeArray[checkX - bottomLeft.x, checkY - bottomLeft.y];
+            int MoveCost = CurNode.G + (CurNode.x - checkX == 0 || CurNode.y - checkY == 0 ? 10 : 14);
+
+
+            // 이동비용이 이웃노드G보다 작거나 또는 열린리스트에 이웃노드가 없다면 G, H, ParentNode를 설정 후 열린리스트에 추가
+            if (MoveCost < NeighborNode.G || !OpenList.Contains(NeighborNode))
+            {
+                NeighborNode.G = MoveCost;
+                NeighborNode.H = (Mathf.Abs(NeighborNode.x - TargetNode.x) + Mathf.Abs(NeighborNode.y - TargetNode.y)) * 10;
+                NeighborNode.ParentNode = CurNode;
+
+                OpenList.Add(NeighborNode);
+            }
+        }
+    }
+
+
+    
+
+    void OnDrawGizmos()
+    {
+        if (FinalNodeList.Count != 0)
+        {
+            for (int i = 0; i < FinalNodeList.Count - 1; i++)
+            {
+                Gizmos.DrawLine(new Vector2(FinalNodeList[i].x, FinalNodeList[i].y), new Vector2(FinalNodeList[i + 1].x, FinalNodeList[i + 1].y));
+            }
+        }
+    }
+}
